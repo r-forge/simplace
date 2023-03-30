34905a5e85aaadb930b76d03b15b57b067de6aae
guntherkrauss (Gunther Krauss) <guntherkrauss@uni-bonn.de>
2023-03-30 07:16:41 +0000

    removed simplaceUtil package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/simplace@57 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/simplaceUtil/R/dummy.R
+++
@@ -1,6 +0,0 @@
-#' @importFrom shinyFiles getVolumes
-#' @importFrom DT coerceValue
-#' @importFrom simplace findFirstSimplaceInstallation
-.mydummy <- function () {
-  coerceValue(getVolumes()(),findFirstSimplaceInstallation())
-}
--- pkg/simplaceUtil/R/run_apps.R
+++
@@ -1,9 +0,0 @@
-#' Runs Shiny app
-#'
-#' @param ... parameters passed to shiny::runApp
-#' @export
-
-runSimplaceGuiApp <- function(...)
-{
-  shiny::runApp(appDir = system.file("shiny","gui", package = "simplaceUtil"),...)
-}
